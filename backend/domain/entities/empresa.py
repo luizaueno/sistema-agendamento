3cc535294d5b378fd05592207d962e91a162118d
luiza <161471348+luizaueno@users.noreply.github.com>
2026-04-07 19:15:50 -0300

    definição e tipo dos atributos, entidade empresa

---
+++ backend/domain/entities/empresa.py
@@ -0,0 +1,7 @@
+class Empresa:
+    def __init__(self, nome, cnpj, max_profissionais, total_admins, id = None):
+        self.id: int = id
+        self.nome: str = nome
+        self.cnpj: int = cnpj
+        self.max_profissionais: int = max_profissionais
+        self.total_admins: int = total_admins
