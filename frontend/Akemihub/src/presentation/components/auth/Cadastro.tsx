ed3328bf221ba1e84b51bb4a1c3f7ed331b39d92
luiza <161471348+luizaueno@users.noreply.github.com>
2026-07-02 17:43:37 -0300

    conectando react ao FastAPI

--- frontend/Akemihub/src/presentation/components/auth/Cadastro.tsx
+++ frontend/Akemihub/src/presentation/components/auth/Cadastro.tsx
@@ -1,42 +1,72 @@
+import React, { useState } from 'react'
 import './cadastro.css'
+import axios from 'axios'
+
 
 interface CadastroProps {
     className: string
 }
 
 function Cadastro({ className }: CadastroProps) {
-    return(
-          <form className={`painel painel-cadastro ${className}`} aria-labelledby="painel-cadastro"> 
-        <h2 id="painel-cadastro" className="cadastro-title">Crie sua Conta</h2> 
-        
-        <fieldset className="form-cadastro-section"> 
-            <legend className="sr-only">Dados da empresa</legend> 
-            <div className="form-cadastro-group">
-                <input className="form-cadastro-input" required placeholder=" " type="text" name="nome" id="nome"/> 
-                <label className="form-cadastro-label" htmlFor="nome">Nome da empresa</label> 
-            </div>
-            <div className="form-cadastro-group">
-                <input className="form-cadastro-input" required placeholder="XX.XXX.XXX/XXXX-XX" type="text" name="cnpj" id="cnpj"/>
-                <label className="form-cadastro-label" htmlFor="cnpj">CNPJ</label>
-            </div>
-        </fieldset>
 
-        <fieldset className="form-cadastro-section">
-            <legend className="sr-only">Dados de acesso</legend>
-            <div className="form-cadastro-group">
-                <input className="form-cadastro-input" required placeholder="email@example.com" type="email" name="email" id="email"/>
-                <label className="form-cadastro-label" htmlFor="email">Email</label>
-            </div>
-            <div className="form-cadastro-group">
-                <input className="form-cadastro-input" required placeholder=" " type="password" name="senha" id="senha"/> 
-                <label className="form-cadastro-label" htmlFor="senha">Senha</label>
-            </div>
-        </fieldset>
+    const [dadosCadastro, setDadosCadastro] = useState({ nome: "", cnpj: "", email: "", senha: "" })
 
-        <button className="form-cadastro-button" type="submit">Cadastrar</button>
-    </form>
+    function HandleChange(event: React.ChangeEvent<HTMLInputElement>) {
+
+        setDadosCadastro({
+            ...dadosCadastro,
+            [event.target.name] : event.target.value
+        })
+    } 
+    
+    async function HandleSubmit(event: React.SyntheticEvent<HTMLFormElement, SubmitEvent>) {
+        event.preventDefault()
+        try {
+            console.log(" Enviando dados para o backend: ")
+
+            await axios.post('http://127.0.0.1:8000/cadastrar', dadosCadastro)
+
+            console.log("Resposta do backend: ")
+            
+        }
+
+        catch {
+            console.log("ERRO: ")
+        }
+     
+    }
+
+    return (
+        <form className={`painel painel-cadastro ${className}`} aria-labelledby="painel-cadastro" onSubmit={HandleSubmit}> 
+            <h2 id="painel-cadastro" className="cadastro-title">Crie sua Conta</h2> 
+            
+            <fieldset className="form-cadastro-section"> 
+                <legend className="sr-only">Dados da empresa</legend> 
+                <div className="form-cadastro-group">
+                    <input className="form-cadastro-input" required placeholder=" " type="text" name="nome" value={dadosCadastro.nome} onChange={HandleChange} id="nome"/> 
+                    <label className="form-cadastro-label" htmlFor="nome">Nome da empresa</label> 
+                </div>
+                <div className="form-cadastro-group">
+                    <input className="form-cadastro-input" required placeholder="XX.XXX.XXX/XXXX-XX" type="text" name="cnpj" value={dadosCadastro.cnpj} onChange={HandleChange} id="cnpj"/>
+                    <label className="form-cadastro-label" htmlFor="cnpj">CNPJ</label>
+                </div>
+            </fieldset>
+
+            <fieldset className="form-cadastro-section">
+                <legend className="sr-only">Dados de acesso</legend>
+                <div className="form-cadastro-group">
+                    <input className="form-cadastro-input" required placeholder="email@example.com" type="email" name="email" value={dadosCadastro.email} onChange={HandleChange} id="email"/>
+                    <label className="form-cadastro-label" htmlFor="email">Email</label>
+                </div>
+                <div className="form-cadastro-group">
+                    <input className="form-cadastro-input" required placeholder=" " type="password" name="senha" value={dadosCadastro.senha} onChange={HandleChange} id="senha"/> 
+                    <label className="form-cadastro-label" htmlFor="senha">Senha</label>
+                </div>
+            </fieldset>
+
+            <button className="form-cadastro-button" type="submit">Cadastrar</button>
+        </form>
     )
 }
-
 
 export default Cadastro
--- frontend/Akemihub/src/presentation/components/auth/Login.tsx
+++ frontend/Akemihub/src/presentation/components/auth/Login.tsx
@@ -1,4 +1,7 @@
+import React, { useState } from 'react'
 import './login.css'
+
+
 
 interface LoginProps {
     className: string
@@ -6,33 +9,33 @@ interface LoginProps {
 
 function Login({ className }: LoginProps) {
 
+    const [dadosLogin, setDadosLogin] = useState({ email: "", senha: ""})
+
+    function HandleChange(event: React.ChangeEvent<HTMLInputElement>) {
+
+        setDadosLogin({
+            ...dadosLogin,
+            [event.target.name] : event.target.value
+        })
+    }
+
+    function HandleSubmit(event: React.SyntheticEvent<HTMLFormElement, SubmitEvent>) {
+
+        event.preventDefault()
+    }
+    
     return (
-        <form className={`painel painel-login ${className}`}
-        aria-labelledby="titulo-login"> 
+        <form className={`painel painel-login ${className}`} aria-labelledby="titulo-login" onSubmit={HandleSubmit}> 
             <fieldset className="login-section">
                 <div className="tela-login-group">
                     <h1 id="titulo-login" className="login-title">Login</h1>
                 </div>
                 <div className="tela-login-group">
-                    <input 
-                        type="email" 
-                        name="email" 
-                        id="login-email" 
-                        className="tela-login-input" 
-                        required 
-                        placeholder="email@example.com"
-                    />
+                    <input type="email" name="email" value={dadosLogin.email} onChange={HandleChange} id="login-email" className="tela-login-input" required placeholder="email@example.com"/>
                     <label className="tela-login-label" htmlFor="login-email">Email</label>
                 </div>
                 <div className="tela-login-group">
-                    <input 
-                        type="password" 
-                        name="senha" 
-                        id="login-senha" 
-                        className="tela-login-input" 
-                        required 
-                        placeholder=" "
-                    />
+                    <input type="password" name="senha" value={dadosLogin.senha} onChange={HandleChange} id="login-senha" className="tela-login-input" required placeholder=" " />
                     <label className="tela-login-label" htmlFor="login-senha">Senha</label>
                 </div>
                 
