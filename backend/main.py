f57caf3234bec6abb55895089871b161d6672264
luiza <161471348+luizaueno@users.noreply.github.com>
2026-07-28 10:26:12 -0300

    arquivo main concluido

--- backend/main.py
+++ backend/main.py
@@ -8,4 +8,4 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI() # aplicacao agora pode receber requisições
 app.add_middleware(CORSMiddleware,  allow_origins =["http://localhost:5173"],  allow_methods = ["POST"],  allow_headers = ["Content-Type", "Accept"])
 app.include_router(cadastrorouter) # incluindo as rotas cadastro
-app.include_router(loginrouter) # e login
+app.include_router(loginrouter) # e login 
